15e9f34c4740b25fc1858c6f27e5980a5a9e0d6b
Mohammed Nabil Abubakar <67646792+Mnet007@users.noreply.github.com>
2025-10-20 20:38:36 +0100

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const navLinks = [
+    [
+        { label: 'Store' },
+        { label: 'Mac' },
+        { label: 'IPhone' },
+        { label: 'Watch' },
+        { label: 'Vision' },
+        { label: 'AirPods' },
+    ]
+]
